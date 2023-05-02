3187e77ec5b564f1f146641254436b0269fb39d5
GustavodRibeiro <gustavo_d_ribeiro@estudante.sesisenai.org.br>
2023-05-02 08:17:08 -0300

    fbfbdbfbdg

--- VirtualCine-main/inserir.php
+++ VirtualCine-main/inserir.php
@@ -1,15 +1,15 @@
-<?php
+<?php 
     include("conecta.php");
-    // Para pegar o texto dos inputs
-    $nome = $_POST["nome"];
-    $cpf      = $_POST["cpf"];
-    $email     = $_POST["email"];
-    $senha     = $_POST["senha"];
 
-    $comando = $pdo->prepare("INSERT INTO usuario(nome,cpf,email,senha) VALUES ('$nome','$cpf', '$email' ,'$senha')");
+    $nome = $_POST["nome"];
+    $cpf = $_POST["cpf"];
+    $email = $_POST["email"];
+    $senha = $_POST["senha"];
+
+    $comando = $pdo->prepare("INSERT INTO usuario(nome,cpf,email,senha) VALUES ('$nome', '$cpf','$email', '$senha')");
 
     $resultado = $comando->execute();
 
-    // Para voltar no formulário
+    //para voltar no fomulário:
     header("Location: cartão.html");
 ?>
